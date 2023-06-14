6a58a880d81a2b17cb06ec858f1bda61c506303a
Darya Stepanova <darya.stepanova95@gmail.com>
2023-06-14 17:55:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import * as serviceWorker from './serviceWorker'
+
 
 
 
@@ -15,7 +15,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-//serviceWorker.unregister();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
